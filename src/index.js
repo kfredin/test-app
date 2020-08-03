20efe78518efe52efda0481d7c12e73fd472521a
Kyle Fredin <kfredin@axcient.com>
2020-08-03 10:34:44 -0600

    add sub function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 const add = (a, b) => a + b;
+const sub = (a, b) => a + b;
 
-module.exports = add;
+module.exports = { add, sub };
--- test/index.js
+++ test/index.js
@@ -1,7 +1,13 @@
-const add = require("../src");
+const { add, sub } = require("../src");
 
 QUnit.module("add", function () {
   QUnit.test("should add two numbers", function (assert) {
     assert.equal(add(1, 1), 2, "1 + 1 = 2");
+  });
+});
+
+QUnit.module("sub", function () {
+  QUnit.test("should subtract two numbers", function (assert) {
+    assert.equal(sub(1, 1), 2, "1 - 1 = 0");
   });
 });
